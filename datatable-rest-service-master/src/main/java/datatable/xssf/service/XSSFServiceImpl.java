4f53638c2b46ad67afb722aa17a9ee172d34a295
max cheung <maxcheung@optusnet.com.au>
2016-08-03 22:31:31 +1000

    row mapper

--- datatable-rest-service-master/src/main/java/datatable/xssf/service/XSSFServiceImpl.java
+++ datatable-rest-service-master/src/main/java/datatable/xssf/service/XSSFServiceImpl.java
@@ -1,9 +1,10 @@
 package datatable.xssf.service;
 
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 
 import org.apache.poi.openxml4j.exceptions.OpenXML4JException;
@@ -15,6 +16,9 @@ import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.stereotype.Component;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import datatable.domain.ClientCode;
 import datatable.domain.ClientCodeValue;
 
@@ -23,10 +27,10 @@ public class XSSFServiceImpl implements XSSFService {
 
 
 	@Override
-	public List<ClientCode> extractData(String clientCd, String fileName) throws IOException, OpenXML4JException {
+	public List<ClientCode> extractData(String clientCd, InputStream ExcelFileToRead) throws IOException, OpenXML4JException {
 		
 		List<ClientCode> ret  = new ArrayList<ClientCode>();
-		InputStream ExcelFileToRead = new FileInputStream(fileName);
+//		InputStream ExcelFileToRead = new FileInputStream(fileName);
 		final OPCPackage pkg = OPCPackage.open(ExcelFileToRead);
 		final Workbook workbook = new XSSFWorkbook(pkg);
 		for (Sheet sheet : workbook) {
@@ -37,6 +41,7 @@ public class XSSFServiceImpl implements XSSFService {
 				String value = getCellValue(row.getCell(1));
 				ClientCodeValue clientCodeValue = createClientCodeValve(clientCode, key, value);
 				clientCode.getClientCodeValues().add(clientCodeValue);
+				mapRow(row);
 			}
 		}
 		workbook.close();
@@ -44,6 +49,34 @@ public class XSSFServiceImpl implements XSSFService {
 	}
 
 
+	private ClientCodeValue mapRow(Row row) throws IOException{
+		
+		
+		List<String> headers = Arrays.asList("codeKey", "codeValue", "La Plata");
+		ClientCodeValue clientCodeValue = new ClientCodeValue();
+		
+		HashMap<String, Object> jsonData = new HashMap<String, Object>();
+
+			for (Cell cell : row){
+				//set foreground color here
+				int i = cell.getColumnIndex();
+				String  cellValue = getCellValue(cell);
+				String coName = headers.get(i);
+				System.out.println(coName + " : " + cellValue);
+				jsonData.put(coName, cellValue);
+			}
+		ObjectMapper objectMapper = new ObjectMapper();
+		String jsonString = objectMapper.writeValueAsString(jsonData);
+	    System.out.println("Json from map : " + jsonString);
+	    clientCodeValue = objectMapper.readValue(jsonString, ClientCodeValue.class);
+	    System.out.println("Json from clientCodeValue : "   + objectMapper.writeValueAsString(clientCodeValue));
+	    
+	    final ClientCodeValue pojo = objectMapper.convertValue(jsonData, ClientCodeValue.class);
+	    
+		return clientCodeValue;
+	}
+	
+	
 	private String  getCellValue(Cell cell) {
 		String val = null;
 		switch (cell.getCellType()) {
@@ -76,6 +109,13 @@ public class XSSFServiceImpl implements XSSFService {
 		clientCodeValue.setCodeKey(codeKey);
 		clientCodeValue.setCodeValue(codeValue);
 		return clientCodeValue;
+	}
+
+
+	@Override
+	public List<ClientCode> extractData(String clientCd, String fileName) throws IOException, OpenXML4JException {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 
